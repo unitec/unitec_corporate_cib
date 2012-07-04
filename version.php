12c38bbe21c81b012b7357a8b386078da2123e2b
Miriam <kihitara@gmail.com>
2012-07-04 18:28:07 +1200

    Version update

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012061200; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2012070400; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011120100; // Requires this Moodle version
 //Note that this plugin may work on earlier versions of Moodle 2 but has only been tested in 2.2
 //To change the version requirement to 2.0, use 2010112400
@@ -18,5 +18,5 @@ $plugin->requires  = 2011120100; // Requires this Moodle version
 //To change the version requirement to 2.2, use 2011120100
 //Note that if you change the version requirement to lower than 2.2, it is at your own risk!
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1 (Build: 2012061200)';
+$plugin->release = '1.1 (Build: 2012070400)';
 $plugin->component = 'theme_unitec_corporate_cib'; // Full name of the plugin (used for diagnostics)
