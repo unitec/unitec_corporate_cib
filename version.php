57a59cc083c2e97a5271214f7f96e1e48086e632
Miriam <kihitara@gmail.com>
2012-10-24 11:39:07 +1300

    Lowered version requirement

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012072400; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011120100; // Requires this Moodle version
+$plugin->requires  = 2010112400; // Requires this Moodle version
 //Note that this plugin may work on earlier versions of Moodle 2 but has only been tested in 2.2
 //To change the version requirement to 2.0, use 2010112400
 //To change the version requirement to 2.1, use 2011070100
